682199e52e6d74385a8e4728e189bc2b1b5f35e3
jack775544 <jack775544@gmail.com>
2020-06-01 13:50:15 +1000

    Initial Commit

---
+++ ScripterTool.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ScripterTool.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ ScripterTool.Reverser/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ScripterTool.Reverser
+{
+    public class Class1
+    {
+    }
+}
